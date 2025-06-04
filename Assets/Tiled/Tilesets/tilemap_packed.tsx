b8b298dbbb7b35ce0e8b4c6e4a641edd4c7a6d28
JGruberB <jgruberb@edu.tecnocampus.cat>
2025-06-04 21:31:59 +0200

    Levels 1-8 Created

--- Assets/Tiled/Tilesets/tilemap_packed.tsx
+++ Assets/Tiled/Tilesets/tilemap_packed.tsx
@@ -111,11 +111,6 @@
    <object id="1" x="0" y="0" width="16.9861" height="16.9472"/>
   </objectgroup>
  </tile>
- <tile id="67">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="2" y="3" width="14" height="13"/>
-  </objectgroup>
- </tile>
  <tile id="76">
   <objectgroup draworder="index" id="2">
    <object id="1" x="1.01111" y="0" width="15.9556" height="16.9889"/>
